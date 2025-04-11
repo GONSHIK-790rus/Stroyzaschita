53a787c7f32dda8b1efacb8c5da66ffbba718750
Pilyugin Egor <pilyugin06@bk.ru>
2025-04-11 23:00:27 +0300

    Начало работы над архитектурой приложения.

---
+++ Stroyzaschita.Domain/Entities/User.cs
@@ -0,0 +1,10 @@
+﻿namespace Stroyzaschita.Domain.Entities;
+
+class User {
+    public Guid Id { get; set; }
+
+    public string Login { get; set; } = string.Empty;
+    public string PasswordHash { get; set; } = string.Empty;
+
+
+}
---
+++ Stroyzaschita.Domain/Entities/UserProfile.cs
@@ -0,0 +1,12 @@
+﻿namespace Stroyzaschita.Domain.Entities;
+
+class UserProfile {
+    public Guid userId {  get; set; }
+
+    public string? Name { get; set; }
+    public string? PhoneNumber { get; set; }
+    public string? Address { get; set; }
+    public byte[]? Avatar { get; set; }
+
+    public User? User { get; set; }
+}
