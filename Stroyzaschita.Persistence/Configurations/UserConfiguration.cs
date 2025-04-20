69f38df21dc8119463682a65e2f90e7cf1387d7c
Pilyugin Egor <pilyugin06@bk.ru>
2025-04-20 22:54:51 +0300

    Добавлены конфигурации

---
+++ Stroyzaschita.Persistence/Configurations/UserConfiguration.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Stroyzaschita.Domain.Entities;
+
+namespace Stroyzaschita.Persistence.Configurations;
+
+public class UserConfiguration : IEntityTypeConfiguration<User> {
+    private const int _MAX_FIELD_LENGTH = 255;
+
+    public void Configure(EntityTypeBuilder<User> builder) {
+        builder.ToTable("users");
+        
+        builder.HasKey(user => user.Id);
+
+        builder.Property(user => user.Id)
+            .HasColumnName("id")
+            .HasMaxLength(_MAX_FIELD_LENGTH)
+            .IsRequired();
+
+        builder.Property(user => user.Login)
+            .HasColumnName("login")
+            .HasMaxLength(_MAX_FIELD_LENGTH)
+            .IsRequired();
+
+        builder.Property(user => user.PasswordHash)
+            .HasColumnName("password")
+            .HasMaxLength(_MAX_FIELD_LENGTH)
+            .IsRequired();
+
+        builder.HasOne(user => user.UserProfile)
+            .WithOne(userProfile => userProfile.User)
+            .HasForeignKey<UserProfile>(userProfile => userProfile.UserId)
+            .OnDelete(DeleteBehavior.Cascade);
+    }
+}
---
+++ Stroyzaschita.Persistence/Configurations/UserProfileConfiguration.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Stroyzaschita.Domain.Entities;
+
+namespace Stroyzaschita.Persistence.Configurations;
+
+class UserProfileConfiguration : IEntityTypeConfiguration<UserProfile> {
+    private const int _MAX_FIELD_LENGTH = 255;
+
+    public void Configure(EntityTypeBuilder<UserProfile> builder) {
+        builder.ToTable("users_data");
+
+        builder.HasKey(userProfile => userProfile.UserId);
+
+        builder.Property(userProfile => userProfile.UserId)
+            .HasColumnName("user_id");
+
+        builder.Property(userProfile => userProfile.Name)
+            .HasColumnName("name")
+            .HasMaxLength(_MAX_FIELD_LENGTH);
+
+        //
+        // TODO: CategoryId, ObjectName, Avatar, ContractExpiredAt. Спать хочу
+
+        builder.Property(userProfile => userProfile.PhoneNumber)
+            .HasColumnName("phone_number")
+            .HasMaxLength(50);
+
+        builder.Property(userProfile => userProfile.Address)
+            .HasColumnName("address")
+            .HasMaxLength(_MAX_FIELD_LENGTH);
+
+    }
+}
